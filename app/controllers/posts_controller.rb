9cb56ce9c32d778b4fcdc24801ea06e7526b6c2a
vagrantape <luckstedcraig@gmail.com>
2015-07-17 21:51:14 -0700

    edit function added

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.create
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
+
+end
